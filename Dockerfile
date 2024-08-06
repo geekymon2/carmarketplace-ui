a16b03fad6565459b4de2d1a267d45a63d811c72
geekymon2 <geekymon2@gmail.com>
2024-08-06 16:48:36 +1000

    fixed environment defect

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ WORKDIR /usr/src/app
 COPY . /usr/src/app
 RUN npm install -g @angular/cli
 RUN npm install
-CMD ["ng", "serve", "--host", "0.0.0.0"]
+CMD ["ng", "serve", "--host", "0.0.0.0", "-c", ${NG_APP_ENVIRONMENT}]
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment.development';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
