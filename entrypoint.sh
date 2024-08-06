6a02d42e14133863654d4eddc4d182250884f60c
geekymon2 <geekymon2@gmail.com>
2024-08-06 18:41:06 +1000

    uypdate

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ source version.properties
 echo "This is the entrypoint script executing nodejs for: $ARTIFACT_NAME"
 echo "Image version: $IMAGE_VERSION"
 echo "Ng Environment: $NG_APP_ENVIRONMENT"
-ng serve --configuration=$NG_APP_ENVIRONMENT
+ng serve --no-watch --configuration=$NG_APP_ENVIRONMENT
