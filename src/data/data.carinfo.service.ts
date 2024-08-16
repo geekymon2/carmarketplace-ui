6a9956fa4f1c4eb4aaed49d5eff3c72b909937d4
geekymon2 <geekymon2@gmail.com>
2024-08-16 16:31:14 +1000

    load config from runtime

--- src/data/data.carinfo.service.ts
+++ src/data/data.carinfo.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { ApiBasePaths } from '../apis/apipaths';
 import { Observable } from 'rxjs/internal/Observable';
+import { ConfigService } from '../config/config.service';
 
 export interface Make {
   id: string;
@@ -20,26 +21,26 @@ export interface BodyType {
   name: string;
 }
 
-const baseUrl = environment.baseUrl + ApiBasePaths.CAR_INFO_SERVICE;
-
 @Injectable({
   providedIn: 'root',
 })
 export class CarInfoDataService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private configService: ConfigService) {}
+
+  private baseUrl = this.configService.apiUrl + ApiBasePaths.CAR_INFO_SERVICE;
 
   getMakes(): Observable<any> {
-    return this.http.get<any>(baseUrl + '/car/makes');
+    return this.http.get<any>(this.baseUrl + '/car/makes');
   }
 
   getBodyTypes(): Observable<any> {
-    return this.http.get<any>(baseUrl + '/car/types');
+    return this.http.get<any>(this.baseUrl + '/car/types');
   }
 
   getModels(make: string, type: string): Observable<any> {
     let params = new HttpParams();
     params = params.append('make', make);
     params = params.append('type', type);
-    return this.http.get<any>(baseUrl + '/car/models', { params });
+    return this.http.get<any>(this.baseUrl + '/car/models', { params });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
+import { ConfigService } from './config/config.service';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-
-console.log("API base url:" + environment.baseUrl);
+fetch(`/assets/config.json`)
+  .then((response) => response.json())
+  .then((config) => {
+    platformBrowserDynamic([{ provide: 'CONFIG', useValue: config }]);
+    bootstrapApplication(AppComponent, appConfig).catch((err) =>
+      console.error(err)
+    );
+    console.log('API base url:' + config.baseUrl);
+  });
